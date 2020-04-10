f4e04a353c587ece6657361aeb857f433ba3705f
Alan Ashworth <awashworth927@gmail.com>
2020-04-10 14:00:05 -0400

    Insert value to schema.sql

--- db/seeds.sql
+++ db/seeds.sql
@@ -1,6 +1,7 @@
 INSERT INTO burgers (burger_name, devoured) 
-VALUES ('Big King XL', false), 
+VALUES ('Big King XL', true), 
         ('Impossible Whopper', false),
         ('Bacon King', false),
         ('BBQ Bacon Whopper', false),
-        ('Double Whopper', false);
+        ('Double Whopper', false),
+        ('Mushroom and Swiss', false);
