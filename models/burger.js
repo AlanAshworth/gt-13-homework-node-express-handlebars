51f137ecd4e5b9ffe9f14192e0625a702138c813
Alan Ashworth <awashworth927@gmail.com>
2020-04-10 06:51:32 -0400

    Add burger model with import and export capability.

---
+++ models/burger.js
@@ -0,0 +1,5 @@
+// Import the ORM to create functions that will interact with the database.
+var orm = require("../config/orm.js");
+
+// Export the database functions for the controller (burgersController.js).
+module.exports = burger;
